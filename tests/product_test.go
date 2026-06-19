32d43891f53c773bcfcab295bee1783ae7b4026e
ellliecar <elizabethcardona993@gmail.com>
2026-06-19 15:54:50 -0500

    Add tests for NewProduct function

--- tests/product_test.go
+++ tests/product_test.go
@@ -1,1 +1,28 @@
+package tests
 
+import (
+	"testing"
+	"ecommerce-go-system/internal/models"
+)
+
+func TestNewProduct(t *testing.T) {
+	tests := []struct {
+		name    string
+		price   float64
+		stock   int
+		wantErr bool
+	}{
+		{"Valid", 100.0, 10, false},
+		{"InvalidPrice", -5.0, 10, true},
+		{"InvalidStock", 50.0, -1, true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := models.NewProduct("T1", "Test", tt.price, tt.stock)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("expected error: %v, got: %v", tt.wantErr, err)
+			}
+		})
+	}
+}
